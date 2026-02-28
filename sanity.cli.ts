507674888069f2eb72384a05708c98df3921ad54
Matt Zurrer <matthias.zuerrer@gmail.com>
2026-02-28 17:54:27 +0100

    feat: update Sanity dependencies and add `appId` to CLI configuration.feat: update Sanity dependencies and add `appId` to CLI configuration.

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,4 +1,4 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
@@ -10,6 +10,7 @@ export default defineCliConfig({
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/cli#auto-updates
      */
+    appId: 'i6i1zexjxetcu5jouc31s2ol',
     autoUpdates: true,
   }
 })
